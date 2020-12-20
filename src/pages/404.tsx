8232e9d39e4a9404c2274b9cce033569b0cf5a80
Bartosz Gleń <33288445+karmek-k@users.noreply.github.com>
2020-12-20 15:52:22 +0100

    Add PageProps to pages

--- src/pages/404.tsx
+++ src/pages/404.tsx
@@ -1,7 +1,8 @@
+import { PageProps } from 'gatsby';
 import React from 'react';
 import Layout from '../components/layout';
 
-const NotFoundPage: React.FC = () => {
+const NotFoundPage: React.FC<PageProps> = () => {
   return (
     <Layout>
       <h1>Page not found</h1>
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,7 +1,8 @@
+import { PageProps } from 'gatsby';
 import React from 'react';
 import Layout from '../components/layout';
 
-const IndexPage: React.FC = () => {
+const IndexPage: React.FC<PageProps> = () => {
   return (
     <Layout>
       <h1>Hello, world!</h1>
