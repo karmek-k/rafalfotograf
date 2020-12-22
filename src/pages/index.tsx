c8f541025d3e39cc5a3c6c81bc8c84d47e7527ac
Bartosz Gleń <33288445+karmek-k@users.noreply.github.com>
2020-12-22 19:52:56 +0100

    Display images on index page

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,11 +1,38 @@
-import { PageProps } from 'gatsby';
+import { graphql, PageProps } from 'gatsby';
 import React from 'react';
+import { Col, Row } from 'react-bootstrap';
 import Layout from '../components/layout';
 
-const IndexPage: React.FC<PageProps> = () => {
+export const query = graphql`
+  {
+    allDatoCmsPhoto {
+      edges {
+        node {
+          id
+          shortText
+          photo {
+            url
+          }
+        }
+      }
+    }
+  }
+`;
+
+const IndexPage: React.FC<PageProps> = ({ data }: any) => {
   return (
     <Layout>
-      <h1>Hello, world!</h1>
+      <Row>
+        {data.allDatoCmsPhoto.edges.map(({ node }) => (
+          <Col sm={3} key={node.id}>
+            <img
+              src={node.photo.url}
+              alt={node.shortText}
+              style={{ maxWidth: '100%', padding: '1em' }}
+            />
+          </Col>
+        ))}
+      </Row>
     </Layout>
   );
 };
